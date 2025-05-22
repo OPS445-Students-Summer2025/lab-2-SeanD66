b65d1865ddba18a99701e2cbddf0a76f703840b4
Sean Daweng <sdaweng@myseneca.ca>
2025-05-22 13:07:41 -0400

    adding lab2c

--- lab2c.py
+++ lab2c.py
@@ -4,14 +4,14 @@
 import sys
 
 
-#name = sys.argv[1]
-#age = sys.argv[2]
-#print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+name = sys.argv[1]
+age = sys.argv[2]
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
 
 
-print(len(sys.argv))
+#print(len(sys.argv))
 
-if len(sys.argv) != 10:
-    print('you do not have 10 arguments')
-    sys.exit
+#if len(sys.argv) != 10:
+#    print('you do not have 10 arguments')
+#    sys.exit
 
